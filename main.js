3f7f3e8fa7a713d0328a6f95c66dcca21e23e50b
Matheus675 <147105431+Matheus675@users.noreply.github.com>
2023-10-05 18:49:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-tutututu
+const idade = 18
+const nome = Don98
+const isEstudante = true
+Console.log (idade, nome, isEstudante)
+//
+//
+const numero1 = 80
+const numero2 = 9
+//
+//
+const soma = numero1 + numero2
+const subtração = numero1 + numero2
+const multiplicação = numero1 * numero2
+const divisão = numero1 / numero2
+console.log (soma, subtração, multiplicação, divisão)
